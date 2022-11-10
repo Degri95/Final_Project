d4c4eb5edccb1192455b918f6df07234043f50db
g0m3zc <106936638+g0m3zc@users.noreply.github.com>
2022-11-10 01:44:04 -0700

    Adds Dash App & update to readme
    Minimal updates to read me.
    Addition of folder with code for dash app.

---
+++ App/pages/pg1.py
@@ -0,0 +1,12 @@
+import dash
+from dash import dcc, html
+import plotly.express as px
+
+dash.register_page(__name__, path='/')
+
+
+layout = html.Div(
+    [
+        dcc.Markdown('US County Health Information')
+    ]
+)
