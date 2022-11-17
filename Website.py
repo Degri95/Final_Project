e13f6178b264004a0e3d8ae2f31e2dadbae05be3
g0m3zc <106936638+g0m3zc@users.noreply.github.com>
2022-11-17 03:14:58 -0700

    updates files for website

--- Website.py
+++ Website.py
@@ -5,23 +5,32 @@ import plotly.express as px
 import json
 import pandas as pd
 from pathlib import Path
-
 from urllib.request import urlopen
 import json
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     counties = json.load(response)
-    
-data = pd.read_csv('/Users/dante/Final_Project/data/health_outcomes_df.csv')
-
+file = Path('data/health_outcomes_df.csv')    
+data = pd.read_csv(file)
 county_df = data
 df = county_df[["CountyFIPS", "CANCER"]]
-
 df["CountyFIPS"] = df["CountyFIPS"].apply(str)
 
+#Import data for mapping
+locationfile = Path('data/mapping_df.csv')  
+mapping_data = pd.read_csv(locationfile)
+mapping_df = county_df.merge(mapping_data, how="inner", on="CountyFIPS")
 
+#Import data for boxplots
+health_status_file = Path('data/health_status_df.csv')  
+status_data = pd.read_csv(health_status_file)
+health_outcomes_file = Path('data/health_outcomes_df.csv')  
+outcomes_data = pd.read_csv(health_outcomes_file)
+health_risk_behaviors_file = Path('data/health_risk_behaviors_df.csv')  
+risk_data = pd.read_csv(health_risk_behaviors_file)
+prevention_file = Path('data/prevention_df.csv')  
+prevention_data = pd.read_csv(prevention_file)
 
 app = dash.Dash(external_stylesheets=[dbc.themes.SANDSTONE])
-
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
     "position": "fixed",
@@ -32,7 +41,6 @@ SIDEBAR_STYLE = {
     "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
 }
-
 # the styles for the main content position it to the right of the sidebar and
 # add some padding.
 CONTENT_STYLE = {
@@ -40,19 +48,20 @@ CONTENT_STYLE = {
     "margin-right": "2rem",
     "padding": "2rem 1rem",
 }
-
 sidebar = html.Div(
     [
-        html.H2("Menu", className="display-4"),
+        html.H2("US Health", className="display-4"),
         html.Hr(),
         html.P(
-            "Visualizations", className="lead"
+            "Counties 2021", className="lead"
         ),
         dbc.Nav(
             [
                 dbc.NavLink("Home", href="/", active="exact"),
-                dbc.NavLink("Page 1", href="/page-1", active="exact"),
-                dbc.NavLink("Page 2", href="/page-2", active="exact"),
+                dbc.NavLink("Dashboard", href="/page-1", active="exact"),
+                dbc.NavLink("Analysis", href="/page-2", active="exact"),
+                dbc.NavLink("Machine Learning", href="/page-3", active="exact"),
+                dbc.NavLink("References", href="/page-4", active="exact"),
             ],
             vertical=True,
             pills=True,
@@ -60,34 +69,50 @@ sidebar = html.Div(
     ],
     style=SIDEBAR_STYLE,
 )
-
 content = html.Div(id="page-content", style=CONTENT_STYLE)
-
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
-
 
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
     if pathname == "/":
-        return [
-            html.H1 ("Map", style={'textAlign':'center'}),
-            dcc.Graph(figure = px.choropleth_mapbox(df, geojson=counties, locations='CountyFIPS', color='CANCER',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 12),
-                           mapbox_style="carto-positron",
-                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
-                           opacity=0.5,
-                           labels={'unemp':'unemployment rate'},
-                           width=1000, height=600
-                          ))]
+        return html.P("Overview", style={'textAlign':'center'})
                      
     elif pathname == "/page-1":
         return [
-            html.H1 ("Graph", style = {'textAlign':'center'}),
+            html.H1 ("Dashboard", style = {'textAlign':'center'}),
             dcc.Dropdown([x for x in df["CountyFIPS"].unique()], id='cont-choice', style={'width':'50%'}),
+            dcc.Graph(figure = px.scatter_mapbox(mapping_df, lat="Lat", lon="Lon", text = 'CountyName', zoom = 2, size = 'total_population', 
+            color = 'CANCER', color_continuous_scale=px.colors.sequential.Viridis,
+            mapbox_style='open-street-map')),
+
+#            dcc.Graph(figure = px.choropleth_mapbox(df, geojson=counties, locations='CountyFIPS', color='CANCER',
+#                           color_continuous_scale="Viridis",
+#                           range_color=(0, 12),
+#                           mapbox_style="carto-positron",
+#                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+#                           opacity=0.5,
+#                           labels={'unemp':'unemployment rate'},
+#                           width=1000, height=600))            
              ]
+
     elif pathname == "/page-2":
-        return html.P("Machine learning", style={'textAlign':'center'})
+        return [ 
+            html.H1("Analysis", style={'textAlign':'center'}),
+            dcc.Graph(figure = px.box(status_data, y=['MHLTH', 'PHLTH', 'GHLTH'] )
+            ),
+            dcc.Graph(figure = px.box(outcomes_data, y=['ARTHRITIS', 'CASTHMA', 'BPHIGH', 'CANCER', 'HIGHCHOL', 'KIDNEY', 'COPD', 'CHD', 'DEPRESSION', 'DIABETES', 'OBESITY', 'TEETHLOST', 'STROKE'] )
+            ),
+            dcc.Graph(figure = px.box(prevention_data, y=['ACCESS', 'CHECKUP', 'DENTAL', 'BPMED', 'CHOLSCREEN', 'MAMMOUSE', 'CERVICAL', 'COLON_SCREEN', 'COREM', 'COREW'] )
+            ),
+            dcc.Graph(figure = px.box(risk_data, y=['BINGE', 'CSMOKING', 'LPA', 'SLEEP'] )
+            ),
+            ]
+
+    elif pathname == "/page-3":
+        return html.H1("Machine learning", style={'textAlign':'center'})
+    elif pathname == "/page-4":
+        return html.H1("References", style={'textAlign':'center'})
+        
     # If the user tries to reach a different page, return a 404 message
     return html.Div(
         [
@@ -97,6 +122,5 @@ def render_page_content(pathname):
         ],
         className="p-3 bg-light rounded-3",
     )
-
 if __name__=='__main__':
     app.run_server(port=8887)
